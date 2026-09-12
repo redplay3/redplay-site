73680275670f50298124d49a3e502306c40415fe
redplay3 <antonschcibria@gmail.com>
2026-09-12 19:48:33 +0300

    Update next.config.ts for structured article routes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/publications/main-replica",
+        destination: "/lineage-2/main/updates/replica",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
